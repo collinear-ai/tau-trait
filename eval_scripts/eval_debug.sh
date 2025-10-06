2c6b2c9b3322d6230b4faf3d6a974509d655d94b
TsachMa <tcmackey.md@gmail>
2025-10-06 21:33:36 +0000

    removed extraneous files and folders

--- eval_scripts/eval_debug.sh
+++
@@ -1,3 +0,0 @@
-python eval_scripts/eval_extensions.py \
- --model-configs eval_scripts/gpt_4o_config.json \
- --results_folder oct_6_debug
--- eval_scripts/eval_gpt.sh
+++
@@ -1,3 +0,0 @@
-python eval_scripts/eval_extensions.py \
- --eval_config_file analysis/notebooks/evals_w_errors_redo_gpt-4o.json \
- --results_folder oct_4_steer_rephraser_evals_w_errors_redo
